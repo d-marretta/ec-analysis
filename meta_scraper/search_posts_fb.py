e232362828961f55f995ea3e25a5bc8c39dfd33d
d-marretta <danielemarretta@gmail.com>
2024-07-12 14:39:02 +0200

    Refactored completely to use mobile site

--- meta_scraper/search_posts_fb.py
+++ meta_scraper/search_posts_fb.py
@@ -3,24 +3,22 @@ sys.path.append('/home/daniele/Documents/Thesis/utils')
 
 import urllib.parse
 from selenium import webdriver
+from selenium.webdriver.common import action_chains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common import keys
 from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import NoSuchElementException,StaleElementReferenceException
+from selenium.common.exceptions import NoSuchElementException,StaleElementReferenceException, TimeoutException
 from time import sleep
 from selenium.webdriver.common.by import By
 import pickle
 from dotenv import load_dotenv
 import json
 import utils
-import base64
-import pytesseract
-from PIL import Image
-import cv2
-import numpy as np
-from string import punctuation
 import dateparser
 from datetime import datetime
 
-SCROLL_AMOUNT = 800
+SCROLL_AMOUNT = 4000
 
 
 def parse_date_str(date):
@@ -33,175 +31,121 @@ def parse_date_str(date):
     else:
         parsed_date = str(dateparser.parse(date, languages=['en'], settings=settings))
     
+    if parsed_date == 'None':
+        return date
+    
     return parsed_date
 
 
-def get_date_str(driver, canvas):
-    canvas_base64 = driver.execute_script("return arguments[0].toDataURL('image/png').substring(22);",canvas)
-
-    canvas_png = base64.b64decode(canvas_base64)
-
-    with open('date.png', 'wb') as f:
-        f.write(canvas_png)
-    
-    img = cv2.imread('date.png')
-
-    white = np.full(img.shape, 255, dtype=np.uint8)
-
-    # Create a mask to identify black pixels
-    black_mask = cv2.inRange(img, lowerb=np.array([0, 0, 0]), upperb=np.array([0, 0, 0]))
-
-    # Change black pixels to white using bitwise operations
-    img = cv2.bitwise_or(white, white, mask=black_mask)
-
-    # Resize and add border
-    img = cv2.copyMakeBorder(img, 30, 30, 30, 30, cv2.BORDER_CONSTANT, value=[255, 255, 255])
-    result = cv2.resize(img,None,fx=3, fy=3, interpolation = cv2.INTER_CUBIC)
-
-    cv2.imwrite('date.png', result)
-    
-    return pytesseract.image_to_string(Image.open('date.png'))
-
-
-
-def get_hashtags_from_text(text):
-    exclude = set(punctuation.replace('#','').replace('@',''))
-    clean_text = ''
-    for ch in text:
-        if ch in exclude:
-            clean_text += ' '
-        else:
-            clean_text += ch
-
-    hashtags = set()
-    mentions = set()
-    words = clean_text.split()
-    for word in words:
-        if word.startswith('@'):
-            cont_mentions = word.strip()[1:].split('@')
-            for ment in cont_mentions:
-                if ment:
-                    mentions.add(f'@{ment}')
-
-        elif word.startswith('#'):
-            cont_hashtags = word.strip()[1:].split('#')
-            for hash in cont_hashtags:
-                if hash:
-                    hashtags.add(f'#{hash.lower()}')
-    
-    return hashtags, mentions
-
 
 def get_post_data(driver, container, query):
-    username= ''
-
-    # Try get normal username
-    try:
-        retry_stale = True
-        attempts = 0
-        while retry_stale and attempts <= 5:
-            attempts += 1
-            try:
-                username = container.find_element(By.XPATH, './/h3//strong').text
-                retry_stale = False
-            except StaleElementReferenceException:
-                continue
-
-
-    except NoSuchElementException:
-        username = ''
-    
-    # If it doesn't work try username of group
-    if not username:
-        try:
-            retry_stale = True
-            attempts = 0
-            while retry_stale and attempts <= 5:
-                attempts += 1
-                try:
-                    username = container.find_element(By.XPATH, './/h3//a[contains(@href, "groups")]/span').text
-                    retry_stale = False
-                except StaleElementReferenceException:
-                    continue
-
-        except NoSuchElementException:
-            return None
-    
-    if not username:
-        return None
-    
-    try:
-        canvas = None
-        retry_stale = True
-        attempts = 0
-        while retry_stale and attempts <= 5:     
-            attempts += 1
-            try:
-                canvas = container.find_element(By.XPATH, './/canvas')
-                retry_stale = False
-            except StaleElementReferenceException:
-                continue
-
-        if canvas:
-            post_date_str = get_date_str(driver, canvas)
-        else:
-            return None
-
-    except NoSuchElementException:
-        return None
-
-    post_date_str = parse_date_str(post_date_str)
-
-    # Get all the text and tags of the post
+    username = ''
+    date = ''
+    text = ''
     mentions = set()
+    hashtags = set()
+
+    retry_stale = True
     try:
-        text_div = container.find_element(By.XPATH, './/div[@data-ad-preview="message" or @data-ad-comet-preview="message"]')
+        while retry_stale:
+            username_and_groups = container.find_elements(By.XPATH, './/div[@data-type="text"]/div[@class="native-text"]/span[@class="rtl-ignore f2 a" and @style="color:#050505;"]')
+            try:
+                for i,name_span in enumerate(username_and_groups):
+                    if i == 0:
+                        username = name_span.text.strip()
+                    else:
+                        mentions.add(name_span.text.strip())
+                retry_stale=False
+            except StaleElementReferenceException:
+                continue
+    except StaleElementReferenceException:
+        return None, True
+    except NoSuchElementException:
+        return None, False
 
-        try:
-            altro_button = text_div.find_element(By.XPATH, './/div[@role="button" and contains(text(), "See more")]')
-            altro_button.click()
-        except NoSuchElementException:
-            altro_button = None
+    
+    try:
+        date = container.find_element(By.XPATH, './/div[@data-type="text"]/div[@class="native-text"]/span[@class="f5" and @style="color:#8a8d91;"]').text[:-2]
+        if date == 'Sponsored':
+            return None,False
+    except NoSuchElementException:
+        pass
+    except StaleElementReferenceException:
+        return None,True
 
-        text_div = container.find_element(By.XPATH, './/div[@data-ad-preview="message" or @data-ad-comet-preview="message"]')
-        
-        try:
-            tags_spans = text_div.find_elements(By.XPATH, './/a[@role="link"]/span')
-            for tag in tags_spans:
-                mentions.add(tag.text)
+    parsed_date = parse_date_str(date)
 
-        except NoSuchElementException:
-            mentions = set()
+    try:
+        see_more = container.find_element(By.XPATH, './/div[@data-type="text"]/div[@class="native-text" and @style="color:#050505;"]/span[@style="color:#65676b;" and contains(text(), "See more")]/../..')
+        #WebDriverWait(driver, 20).until_not(expected_conditions.presence_of_element_located((By.XPATH,'.//div[@data-type="text"]/div[@class="native-text" and @style="color:#050505;"]/span[@style="color:#65676b;" and contains(text(), "See more")]')))
+        driver.execute_script("arguments[0].click();",see_more)
+        sleep(5)
+        return None,True
+    except NoSuchElementException:
+        pass
+    except StaleElementReferenceException:
+        return None,True
 
-        text = text_div.text
+
+    try:
+        retry_stale = True
+        while retry_stale:
+            text_div = container.find_element(By.XPATH, './/div[@data-type="text"]/div[@class="native-text" and @style="color:#050505;"]')
+            try:
+                text = text_div.text
+                retry_stale = False
+            except StaleElementReferenceException:
+                continue
+    except NoSuchElementException:
+        return None, False
+
+    try:
+        #tag_spans = container.find_elements(By.XPATH, './/div[@data-type="text"]/div[@class="native-text" and @style="color:#050505;"]/span[@class="f2 a" and @style="color:#1763cf;"]')
+        tag_spans = WebDriverWait(container,2,ignored_exceptions=(StaleElementReferenceException)).until(expected_conditions.presence_of_all_elements_located((By.XPATH, './/div[@data-type="text"]/div[@class="native-text" and @style="color:#050505;"]/span[@class="f2 a" and @style="color:#1763cf;"]')))
+        for tag in tag_spans:
+            tag_text = tag.text
+            if '/' in tag_text or '.' in tag_text or 'https' in tag_text:
+                continue
+            elif tag_text.startswith("#"):
+                hashtags.add(tag_text.strip())
+            else:
+                mentions.add(tag_text.strip())
+                
 
     except NoSuchElementException:
-        return None
-        
-    hashtags, at_mentions = get_hashtags_from_text(text)
-    if at_mentions:
-        mentions.update(at_mentions)
+        pass
+
+    except TimeoutException:
+        pass
+
 
     post = {
-        'id':username+post_date_str+str(len(text)),
+        'id':username+text,
         'keyword':query,
         'username':username,
-        'date':post_date_str,
+        'date':parsed_date,
         'text':text,
         'mentions':list(mentions),
         'hashtags':list(hashtags)
     }
 
     # print(post)
-    return post
+    return post, False
+
+def click_posts(driver):
+    posts_button = driver.find_element(By.XPATH, '//div[@role="button" and @aria-label="Posts"]')
+    posts_button.click()
 
 
-
-def search_posts(driver, save_dir, url, query, max_posts, post_ids, starting_n):
+def search_posts(driver, save_dir, url, query, max_posts, post_ids, starting_n, ac):
 
     # Allow the page to load
     driver.get(url)
     sleep(5)
+
+    click_posts(driver)
+
+    sleep(4)
 
     print(f'Page url: {url}')
     print(f'Keyword: {query}\n')
@@ -210,24 +154,31 @@ def search_posts(driver, save_dir, url, query, max_posts, post_ids, starting_n):
     post_datas = []   # List of dicts, each dict will be data of a post
     session_ids = set()
 
-    height = 0
-    last_position = 0
     scrolling = True
+    last_position = 0  # Current scroll amount
+    height = 0         # Height to scroll to
 
     already_seen = 0
     skipped = 0
 
-    while scrolling:
-        containers = driver.find_elements(By.XPATH, '//div[@style="border-radius: max(0px, min(var(--card-corner-radius), calc((100vw - 4px - 100%) * 9999))) / var(--card-corner-radius);"]')
+    scrolling_elem = driver.find_element(By.XPATH, '//div[@class="m vscroller"]')
+    sleep(1)
+
+    while True:
+        containers = driver.find_elements(By.XPATH, '//div[contains(@class, "m") and contains(@class, "displayed")]')
         for container in containers:
-            post_data = get_post_data(driver, container, query)
-                
+            post_data, see_more_pressed = get_post_data(driver, container, query)
+            if see_more_pressed:
+                continue
             if post_data:
                 id = post_data['id']
                 if (id not in session_ids) and (id not in post_ids):
                     session_ids.add(id)
                     post_datas.append(post_data)
                     post_counter += 1
+                    with open(save_dir+'/post_'+str(starting_n)+'.json', mode='w', encoding='utf-8') as outjson:
+                        json.dump(post_data, outjson, indent=2)
+                    starting_n += 1
                     print(f'\rGot {post_counter} post(s) out of {max_posts}; Skipped: {skipped}; Already seen: {already_seen}', end="")
                 else:
                     already_seen += 1
@@ -241,44 +192,65 @@ def search_posts(driver, save_dir, url, query, max_posts, post_ids, starting_n):
             print(f'\nCollected {post_counter} posts')
             break
 
-         # Scroll and update last position, scrolling flag and height to scroll to
         height += SCROLL_AMOUNT
-        last_position, scrolling = utils.scroll(driver=driver,height=height,max_attempts=4, last_pos=last_position)
+        last_position, scrolling = scroll(driver=driver,height=height,max_attempts=2, last_pos=last_position, scroller=scrolling_elem, ac=ac)
         if not scrolling:
             print(f'\nCollected {post_counter} posts')
+            break   
 
-    print()
-
-    sleep(5)
-
-    for data in post_datas:
-        with open(save_dir+'/post_'+str(starting_n)+'.json', mode='w', encoding='utf-8') as outjson:
-            json.dump(data, outjson, indent=2)
-        starting_n += 1
+    print()        
     
     return post_counter, session_ids
 
+def scroll(driver, height, max_attempts, last_pos, scroller, ac):
+    # Try scrolling multiple times, in case the page has to load
+    attempt = 0
+    curr_pos = 0
+    while attempt < max_attempts:
+        try:
+            driver.execute_script(f'arguments[0].scrollTo(0, {height})', scroller)
+            sleep(1)
+            curr_pos = driver.execute_script("return arguments[0].scrollTop", scroller)
+            if last_pos == curr_pos or (abs(curr_pos-last_pos) < 10):
+                ac.move_by_offset(0,-925)
+                ac.click_and_hold().perform()
+                sleep(15)
+                ac.release().perform()
 
+                ac.move_by_offset(0,925)
+                ac.click_and_hold().perform()
+                sleep(15)
+                ac.release().perform()
+                attempt += 1
+            else:
+                # Return new last_position, new height to scroll to and 
+                # whether scrolling is still true or false
+                return curr_pos,True
+        except StaleElementReferenceException:
+            scroller = driver.find_element(By.XPATH, '//div[@class="m vscroller"]')
+
+    return curr_pos,False
 
 def setup():
     # Start chrom webdriver
     options = Options()
-    options.add_argument('--headless')
+    #options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--user-agent=Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19')
     driver = webdriver.Chrome(options=options)
-    driver.get("https://www.facebook.com/")
+    driver.get("https://m.facebook.com/")
 
     # Get logins cookies
     cookies = pickle.load(open("../auth_facebook.pkl", "rb"))
     for cookie in cookies:
         driver.add_cookie(cookie)
-    driver.get("https://www.facebook.com/")
+    driver.get("https://m.facebook.com/")
 
     driver.implicitly_wait(2)
     driver.maximize_window()
 
-    sleep(2)
+    sleep(3)
     print('Scraping started')
 
     return driver
@@ -293,18 +265,16 @@ def main():
     print('Already collected: '+str(starting_n)+' files')
 
     keywords = utils.get_keywords(KEYWORDS_DIR)
-    
+
+    ac = action_chains.ActionChains(driver)
+    ac.move_by_offset(1661,980)
+
     for keyword in keywords:
         keyword = keyword.lower().strip()
-        keyword = 'energy communities'
-
-        # url = urllib.parse.quote(string=f"https://www.facebook.com/search/posts/?q={keyword}", safe='/&?=:')
-        # n_posts, new_ids = search_posts(driver, POSTS_DIR, url, keyword, 15, post_ids, starting_n)
-        # starting_n += n_posts
-        # post_ids.update(new_ids)
-
-        url = urllib.parse.quote(string=f"https://www.facebook.com/search/top?q={keyword}", safe='/&?=:')
-        n_posts, new_ids = search_posts(driver, POSTS_DIR, url, keyword, 1, post_ids, starting_n)
+        if keyword == "renewable energy cooperatives":
+            break
+        url = urllib.parse.quote(string=f"https://m.facebook.com/search_results/?q={keyword}", safe='/&?=:')
+        n_posts, new_ids = search_posts(driver, POSTS_DIR, url, keyword, 300, post_ids, starting_n, ac)
         starting_n += n_posts
         post_ids.update(new_ids)
     
